42199828ce428723c64d6d2b1298e2ecdbdc3ace
zakurato <stiven_zeledon@hotmail.com>
2023-08-30 13:00:58 -0600

    cambios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -90,8 +90,8 @@ class HomeController extends Controller
             $productosSeccionCategoria = CreateSeccionProductoCategory::all();
             
 
-            if (isset($top5IdProductosMasVendidos[0]) && isset($top5IdProductosMasVendidos[1]) &&
-                isset($top5IdProductosMasVendidos[2]) && isset($top5IdProductosMasVendidos[3])) {
+            if (isset($top5IdProductosMasVendidos[0]) || isset($top5IdProductosMasVendidos[1]) ||
+                isset($top5IdProductosMasVendidos[2]) || isset($top5IdProductosMasVendidos[3])) {
                                     
                 $productosMasVendidos = Producto::whereIn('id', [
                                      $top5IdProductosMasVendidos[0],
@@ -239,8 +239,8 @@ class HomeController extends Controller
             ->pluck('idFKProducto');
            
 
-            if (isset($top5IdProductosMasVendidos[0]) && isset($top5IdProductosMasVendidos[1]) &&
-                 isset($top5IdProductosMasVendidos[2]) && isset($top5IdProductosMasVendidos[3])) {
+            if (isset($top5IdProductosMasVendidos[0]) || isset($top5IdProductosMasVendidos[1]) ||
+                 isset($top5IdProductosMasVendidos[2]) || isset($top5IdProductosMasVendidos[3])) {
                                               
             $productosMasVendidos = Producto::whereIn('id', [
                                                       $top5IdProductosMasVendidos[0],
