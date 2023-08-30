d03e23ebd33a16d0f724de6fc628e2d457a2afe2
zakurato <stiven_zeledon@hotmail.com>
2023-08-30 10:31:10 -0600

    cambios

--- resources/views/carrito/carritoCompras.blade.php
+++ resources/views/carrito/carritoCompras.blade.php
@@ -6,13 +6,14 @@
     <title>Carrito Compras MagicSexShop</title>
     <meta name="description" content="">
     <meta name="keywords" content="">
-
-
-    <!--/Css propios public-->
+    <link rel="stylesheet" href="{{ asset('carrito/carritoForm.Css') }}?v={{ time() }}" type="text/css"
+        media="all">
     <link rel="stylesheet" href="{{ asset('index/styles.Css') }}?v={{ time() }}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
 </head>
 
 <body id="cart"
