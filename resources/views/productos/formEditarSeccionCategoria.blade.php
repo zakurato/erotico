5e904989b20b88bdc2ad980c5ed7e8dcba6f0803
zakurato <stiven_zeledon@hotmail.com>
2023-09-05 14:15:09 -0600

    cambios

--- resources/views/productos/formEditarSeccionCategoria.blade.php
+++ resources/views/productos/formEditarSeccionCategoria.blade.php
@@ -6,21 +6,25 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Form editar producto seccion categoria</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
     <link rel="stylesheet" href="{{ asset('login/loginAdentro.Css?1.0') }}">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 
 <body>
 
-    <div class="navbar">
-        <ul>
-            <li><a href="{{ route('seccionImagenesCategoria') }}">Inicio</a></li>
-            <!-- Agrega aquí más elementos del navbar si es necesario -->
-        </ul>
-    </div>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a class="navbar-brand" href="{{ route('seccionImagenesCategoria') }}">Inicio</a>
+            </div>
+        </div><!-- /.container-fluid -->
+    </nav>
+    
     <div class="container">
         <br><br>
 
