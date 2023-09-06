69b15546dc876a3a5d41ebe5bd3e2c9d2f46d365
zakurato <stiven_zeledon@hotmail.com>
2023-09-06 15:28:08 -0600

    cambios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -141,16 +141,7 @@ class HomeController extends Controller
                     $productos = Producto::paginate(12);
                     $fotos = Foto::all();
 
-                    //solo la posicion 0
-                    for ($i = 0; $i < min(count($top5IdProductosMasVendidos), 4); $i++) {
-                        if (isset($top5IdProductosMasVendidos[$i])) {
-                            $productosMasVendidos[] = $top5IdProductosMasVendidos[$i];
-                        }
-                    }
-                    
-                    if (!empty($productosMasVendidos)) {
-                        $productosMasVendidos = Producto::whereIn('id', $productosMasVendidos)->get();
-                    }
+                    $productosMasVendidos = [];
 
                     return view("paginaPrincipal.index2",compact("productos","categorias","contadorCarrito","fotos","sessionCliente","productosMasVendidos","productosSeccionCategoria","top5ProductosMasActuales","productoTemporada","imagenPrincipal"));
                 }
