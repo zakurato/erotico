dda684a25a39f252c9c10897b7bee6b224b3ea6d
zakurato <stiven_zeledon@hotmail.com>
2023-10-11 14:21:56 -0600

    cambios

--- resources/views/carrito/factura.blade.php
+++ resources/views/carrito/factura.blade.php
@@ -61,7 +61,7 @@
                     <div class="footer__block-item footer__block-item--text">
                         <button class="footer__title heading h6" aria-expanded="false" aria-controls="block-footer-0"
                             data-action="toggle-collapsible" disabled="disabled">
-                            <span>Acerca de nosotros</span>
+                            <span style="color: black">Acerca de nosotros</span>
                         </button>
                         <p>
                             En ShopisCr, nuestra misión es ofrecer una amplia variedad de productos de alta calidad
