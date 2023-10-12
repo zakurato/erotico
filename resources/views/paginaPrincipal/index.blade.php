7bb1aac602996ed8637d43df94087150d9689dfd
zakurato <stiven_zeledon@hotmail.com>
2023-10-12 16:05:14 -0600

    cambios

--- resources/views/paginaPrincipal/index.blade.php
+++ resources/views/paginaPrincipal/index.blade.php
@@ -10,7 +10,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Readex+Pro&display=swap" rel="stylesheet">
     <!--animaciones-->
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('index/privacidad.Css') }}">
+    <link rel="stylesheet" href="{{ asset('index/privacidad.Css') }}?v={{ time() }}">
+
     <!--iconos-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <!--Bootstrap-->
