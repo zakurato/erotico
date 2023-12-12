c1c6ca06f95066f9d571cd9251832e39e55e5dde
zakurato <stiven_zeledon@hotmail.com>
2023-12-12 14:46:59 -0600

    cambios

--- resources/views/paginaPrincipal/index2.blade.php
+++ resources/views/paginaPrincipal/index2.blade.php
@@ -39,10 +39,6 @@
 <body class="warehouse--v4 features--animate-zoom template-index" data-instant-intensity="viewport">
     <!-- END sections: header-group -->
     <!-- BEGIN sections: overlay-group -->
-    <br>
-    <div>
-        <h1>{{$sessionCliente}}</h1>
-    </div>
     <div class="navbar navbar-inverse"
         style="background-color: #e7e7e7 !important; position: fixed; width: 100% !important; z-index: 9999 !important;">
         <div class="container-fluid">
